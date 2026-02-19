70a5029d6a795e38d90d504ae2c68a6af9f0ed87
Taha Nejad <paprikataha@gmail.com>
2026-02-19 04:24:13 -0800

    Update to 0.3.0

--- zedx.rb
+++ zedx.rb
@@ -4,20 +4,20 @@
 class Zedx < Formula
   desc 'Boilerplate generator for Zed Editor extensions.'
   homepage 'https://github.com/tahayvr/zedx'
-  version '0.2.0'
-  url 'https://registry.npmjs.org/zedx/-/zedx-0.2.0.tgz'
-  sha256 'eba19c18ebfa547e767a37a89c3cfc9c76941a26862888a5ba16a7997f97294a'
+  version '0.3.0'
+  url 'https://registry.npmjs.org/zedx/-/zedx-0.3.0.tgz'
+  sha256 '75a56271f6a43027d1e5735a2ae75ff59193e15b855830ecb0a10fb880a40502'
   license 'Apache-2.0'
 
   depends_on 'node'
 
   def install
-    system "npm", "install", *std_npm_args
-    bin.install_symlink libexec.glob("bin/*")
+    system 'npm', 'install', *std_npm_args
+    bin.install_symlink libexec.glob('bin/*')
   end
 
   test do
     assert_match 'Boilerplate generator for Zed Editor extensions.',
-shell_output("#{bin}/zedx --help")
+                 shell_output("#{bin}/zedx --help")
   end
 end
