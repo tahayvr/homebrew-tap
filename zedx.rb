64ce397c45eee263212a1e4d0401975d6bdda0ad
Taha Nejad <paprikataha@gmail.com>
2026-03-29 16:20:38 -0700

    Update zedx to version 0.8.0

--- zedx.rb
+++ zedx.rb
@@ -4,9 +4,9 @@
 class Zedx < Formula
   desc 'Boilerplate generator for Zed Editor extensions.'
   homepage 'https://github.com/tahayvr/zedx'
-  version '0.7.0'
-  url 'https://registry.npmjs.org/zedx/-/zedx-0.7.0.tgz'
-  sha256 'ed2397aa04618d8cf01e6e9d55e4ba539af2de8b4eed68d17fb8f246f5e1b473'
+  version '0.8.0'
+  url 'https://registry.npmjs.org/zedx/-/zedx-0.8.0.tgz'
+  sha256 'f76c991892ac2eb454b3ca563b45e5e17043ef93dc68867915bc28ca57e004b6'
   license 'Apache-2.0'
 
   depends_on 'node'
