879dc003d0478685e0d5a36036dc815bd525dd41
tahayvr <paprikataha@gmail.com>
2026-04-07 06:39:52 -0700

    Update zedx to version 0.12.0

--- zedx.rb
+++ zedx.rb
@@ -4,9 +4,9 @@
 class Zedx < Formula
   desc 'The CLI toolkit for Zed Editor.'
   homepage 'https://github.com/tahayvr/zedx'
-  version '0.11.0'
-  url 'https://registry.npmjs.org/zedx/-/zedx-0.11.0.tgz'
-  sha256 '4c30610f1b00153016491dda7bdd8b02cdae8ca19f21eb0c66581f208c4960ab'
+  version '0.12.0'
+  url 'https://registry.npmjs.org/zedx/-/zedx-0.12.0.tgz'
+  sha256 'e962ca3a95a6c9a54998a971405a3c3eda2d00c9dec3f9e17808b6e095b687bc'
   license 'Apache-2.0'
 
   depends_on 'node'
