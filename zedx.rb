ad7aaab2fbddb576ebacf3a8d1111877ddb8456f
tahayvr <paprikataha@gmail.com>
2026-05-07 16:40:52 -0700

    Update zedx to version 0.13.1 and correct SHA256 checksum

--- zedx.rb
+++ zedx.rb
@@ -6,7 +6,7 @@ class Zedx < Formula
   homepage 'https://github.com/tahayvr/zedx'
   version '0.13.1'
   url 'https://registry.npmjs.org/zedx/-/zedx-0.13.1.tgz'
-  sha256 'c8d3eae160a892e32837db3dcae515e843e5383fef52b8141940c8bcf8b6d59f'
+  sha256 'a02cb9bf272cc5e648ce3b75b85cab4f217936d88acfdfb8b42d977d54ea4e71'
   license 'Apache-2.0'
 
   depends_on 'node'
